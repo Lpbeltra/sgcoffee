3cd0d21fea86ed3176d050a08b59af0d086e5fc4
Lpbeltra <77076216+Lpbeltra@users.noreply.github.com>
2025-03-05 18:00:34 -0300

    Update api.py

--- api.py
+++ api.py
@@ -5,11 +5,11 @@ import argparse
 app = Flask(__name__)
 
 db_config = {
-    'host': 'xxxxxx',
-    'port': 'xxxxxx',
-    'user': 'xxxxxx',
-    'password': 'xxxxxx',
-    'database': 'xxxxxx'
+    'host': '192.168.1.252',
+    'port': '3306',
+    'user': 'sgps',
+    'password': '6Cx8A3p7',
+    'database': 'sgps'
 }
 
 def get_db_connection():
@@ -19,40 +19,109 @@ def get_db_connection():
 def verificar_login():
     data = request.get_json()
     unique_id = data.get('id_user')
+    nome = data.get('nome')
 
-    if not unique_id:
-        return jsonify({"success": False, "message": "ID do usuário não fornecido."}), 400
+    if unique_id:
+        try:
+            connection = get_db_connection()
+            cursor = connection.cursor(dictionary=True)
 
-    try:
-        connection = get_db_connection()
-        cursor = connection.cursor(dictionary=True)
+            query = "SELECT * FROM webusuario WHERE fkIdVendedor = %s and dataInativacao IS NULL"
+            cursor.execute(query, (unique_id,))
+            user = cursor.fetchone()
 
-        query = "SELECT * FROM webusuario WHERE fkIdVendedor = %s and dataInativacao IS NULL"
-        cursor.execute(query, (unique_id,))
-        user = cursor.fetchone()
+            if user:
+                return jsonify({
+                    "success": True,
+                    "nome": user['nome'],
+                })
+            else:
+                return jsonify({
+                    "success": False,
+                    "message": "Usuário não encontrado."
+                }), 404
 
-        if user:
-            return jsonify({
-                "success": True,
-                "nome": user['nome'],
-            })
-        else:
+        except mysql.connector.Error as err:
+            print(f"Erro no banco de dados: {err}")
             return jsonify({
                 "success": False,
-                "message": "Usuário não encontrado."
-            }), 404
+                "message": "Erro no banco de dados."
+            }), 500
 
-    except mysql.connector.Error as err:
-        print(f"Erro no banco de dados: {err}")
-        return jsonify({
-            "success": False,
-            "message": "Erro no banco de dados."
-        }), 500
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
 
-    finally:
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
+    else:
+        try:
+            connection = get_db_connection()
+            cursor = connection.cursor(dictionary=True)
+            nome_query = '%' + nome + '%'
+            query = "SELECT nome, fkIdVendedor FROM webusuario WHERE nome like %s and dataInativacao IS NULL"
+            cursor.execute(query, (nome_query,))
+            user = cursor.fetchall()
+            print(user)
+            if user:
+                return jsonify({
+                    "success": True,
+                    "data": user,
+                })
+            else:
+                return jsonify({
+                    "success": False,
+                    "message": "Usuário não encontrado."
+                }), 404
+            
+        except mysql.connector.Error as err:
+            print(f"Erro no banco de dados: {err}")
+            return jsonify({
+                "success": False,
+                "message": "Erro no banco de dados."
+            }), 500
+
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+
+@app.route('/listar_usuarios', methods=['POST'])
+def listar_usuarios():
+
+    data = request.get_json()
+    nome = data.get('nome')
+
+    if nome:
+        try:
+            connection = get_db_connection()
+            cursor = connection.cursor(dictionary=True)
+            nome_query = '%' + nome + '%'
+            query = """SELECT nome FROM webusuario WHERE nome LIKE %s AND dataInativacao IS NULL AND nome NOT LIKE '%\\_%' ESCAPE '\\\\'"""
+            cursor.execute(query, (nome_query,))
+            user = cursor.fetchall()
+
+            if user:
+                return jsonify({
+                    "success": True,
+                    "data": user,
+                })
+            else:
+                return jsonify({
+                    "success": False,
+                    "message": "Usuário não encontrado."
+                }), 404
+            
+        except mysql.connector.Error as err:
+            print(f"Erro no banco de dados: {err}")
+            return jsonify({
+                "success": False,
+                "message": "Erro no banco de dados."
+            }), 500
+
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
 
 @app.route('/consulta_saldo', methods=['POST'])
 def consulta_saldo():
@@ -99,16 +168,25 @@ def cafe():
     unique_id = data.get('id_user')
     valor = data.get('valor')
     nome = data.get('nome')
+    descricao = data.get('descricao')
+    nomeLimitado = nome[:29]
 
     if not unique_id or not valor:
         return jsonify({"success": False, "message": "Dados incompletos."}), 400
+    
+
 
     try:
         connection = get_db_connection()
         cursor = connection.cursor(dictionary=True)
-
-        query = """INSERT INTO sgps.cafe (idfunc, nome, valor, data, hora) VALUES (%s, %s, %s, CURDATE(), CURTIME())"""
-        cursor.execute(query, (unique_id, nome, valor))
+        if descricao:
+            print('descricao')
+            query = """INSERT INTO sgps.cafe (idfunc, nome, valor, descricao, data, hora) VALUES (999999, %s, %s, %s, CURDATE(), CURTIME())"""
+            cursor.execute(query, (nomeLimitado, valor, descricao))
+        else: 
+            print('sem descricao')
+            query = """INSERT INTO sgps.cafe (idfunc, nome, valor, data, hora) VALUES (%s, %s, %s, CURDATE(), CURTIME())"""
+            cursor.execute(query, (unique_id, nomeLimitado, valor))
 
         connection.commit()
 
